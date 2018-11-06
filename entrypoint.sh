ef9746f78421c1d1bfe51ec67575112ae0383a28
Niels van Esch <info@nvanesch.nl>
2018-11-06 12:13:03 +0100

    Added filename in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
 
-wget ${1} &> /dev/null
+wget ${1} -O docs.json &> /dev/null
 node_modules/api-spec-converter/bin/api-spec-converter -f openapi_3 -t swagger_2 -s json docs.json
 rm -rf docs.json
